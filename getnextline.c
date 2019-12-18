b7df52a1fe066a56afd9ca48646d504e1fb09acd
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2019-12-18 22:11:21 +0300

    Add files via upload

--- getnextline.c
+++ getnextline.c
@@ -6,7 +6,7 @@
 /*   By: iromero- <iromero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 14:56:35 by iromero-          #+#    #+#             */
-/*   Updated: 2019/12/18 19:52:09 by iromero-         ###   ########.fr       */
+/*   Updated: 2019/12/18 20:11:08 by iromero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,4 +71,59 @@ char	*ft_strdup(const char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
+
+static	int	getcharpls(char **line, char **aux, int n)
+{
+	char *temp;
+	char *temp2;
+
+	if (n < 0)
+		return (-1);
+	if (!n && !*aux)
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	if ((temp = ft_strchr(*aux, '\n')))
+	{
+		*temp = 0;
+		*line = ft_strdup(*aux);
+		temp2 = ft_strdup(++temp);
+		free(*aux);
+		*aux = temp2;
+	}
+	else
+	{
+		*line = *aux;
+		*aux = NULL;
+		return (0);
+	}
+	return (1);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	char	buffer[BUFFER_SIZE +1];
+	static char *aux[4096];
+	int	n;
+	char	*temp;
+
+	if(fd < 0 || BUFFER_SIZE < 1 || !line)
+		return (-1);
+	while ((n = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[n] = 0;
+		if (!aux[fd])
+			aux[fd] = ft_strdup(buffer);
+		else
+		{
+			temp = ft_strjoin(aux[fd], buffer);
+			free(aux[fd]);
+			aux[fd] = temp;
+		}
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (getcharpls(line, &aux[fd], n));
 }
